6708ec14b02340ca30fdc2726d129ca297e56a4a
Lewis Campbell <5160938+lewismcampbell@users.noreply.github.com>
2021-01-07 10:01:26 +0000

    updated to automatically redirect and create files.

--- createLinks.js
+++ createLinks.js
@@ -2,26 +2,25 @@ const fs = require('fs').promises;
 let data;
 
 async function readFile() {
-    try {
-      let json = await fs.readFile('./data/data.json');
-      data = JSON.parse(json);
-    } catch (error) {
-      console.error(`Got an error trying to read the file: ${error.message}`);
-    }
+  try {
+    let json = await fs.readFile('./data/data.json');
+    data = JSON.parse(json);
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
 }
 
 async function openFile(json) {
-    try {
-      await fs.writeFile(`./public/${json.name}.html`, 'name,quantity,price');
-    } catch (error) {
-      console.error(`Got an error trying to write to a file: ${error.message}`);
-    }
+  try {
+    await fs.writeFile(`./public/${json.name}.html`, 'name,111quantity,price21');
+  } catch (error) {
+    console.error(`Got an error trying to write to a file: ${error.message}`);
   }
+}
 
-  (async function () {
-    await readFile();
+(async function () {
+  await readFile();
   data.forEach(obj => {
-        openFile(obj);
-    });
-    //await openFile();
-  })();
+      openFile(obj);
+  });
+})();
